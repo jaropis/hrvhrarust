cd730ddce2f7ace854e5e261fa3f1e275d17d363
Jaroslaw Piskorski <jaropis@zg.home.pl>
2026-07-22 14:28:52 +0200

    some explanations

--- tests/consistency_tests.rs
+++ tests/consistency_tests.rs
@@ -4,9 +4,13 @@ use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
 use hrvhra_rust::asym::AsymVarDesc;
-use hrvhra_rust::common::Annotations;
 use hrvhra_rust::data_reader::RRSeries;
-use hrvhra_rust::runs::RRRuns;
+
+/// These tests test the consistency of the rust results with results from the R version of HRAEpxlorer +
+/// the correct sdnn definition, i.e.
+///   n <- length(new_data$rri[new_data$annot == 0]) # filtered length
+///  SDNN3 <- sd(new_data$rri[new_data$annot == 0]) * sqrt((n-1)/n)
+/// which is not actually calculated in the R version
 
 /// Reads a result file whose non-empty lines contain a metric name and a numeric value.
 ///
